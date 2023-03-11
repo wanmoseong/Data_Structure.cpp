ab8505a528e4ef4baab2fd159f4b59009761ea42
Wanmo <97231408+wanmoseong@users.noreply.github.com>
2023-03-11 16:52:14 +0900

    Create CH1_Array_as_a_parameter_fo_a_function.cpp

---
+++ CH1_Array_as_a_parameter_fo_a_function.cpp
@@ -0,0 +1,19 @@
+# Data_Structure.cpp
+
+#include <cstdio>
+#define MAX_SIZE 10
+
+void sub(int x, int arr[]) {
+	x = 10;
+	arr[0] = 10;
+}
+
+void main() {
+	int var;
+	int list[MAX_SIZE];
+
+	var = 0;
+	list[0] = 0;
+	sub(var, list);
+	printf("var=%d, list[0]=%d\n", var, list[0]);
+}
